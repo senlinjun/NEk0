0e7b7bd0057c18bc344798a34232de6f8febf2b0
senlinjun <yu2755221817@163.com>
2026-05-09 13:54:28 +0800

    pre_build script

---
+++ pre_build.py
@@ -0,0 +1,9 @@
+import subprocess,os,shutil
+os.chdir("native")
+process1 = subprocess.Popen("cargo build --release --target x86_64-linux-android")
+process2 = subprocess.Popen("cargo build --release --target aarch64-linux-android")
+process1.wait()
+process2.wait()
+os.chdir("..")
+shutil.copy("native/target/x86_64-linux-android/release/libtsclient.so","android/app/src/main/jniLibs/x86_64/libtsclient.so")
+shutil.copy("native/target/aarch64-linux-android/release/libtsclient.so","android/app/src/main/jniLibs/arm64-v8a/libtsclient.so")
